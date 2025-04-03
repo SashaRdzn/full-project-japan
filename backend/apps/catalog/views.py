1250545d4de236ca15e9a14b116bbe26af3296db
Рудзинский Александр <rudzinskijsasa@gmail.com>
2025-04-04 00:57:30 +0300

    Catalog ^_^

--- backend/apps/catalog/views.py
+++ backend/apps/catalog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Card, Category, Image
+from .serializers import CardSerializer, CategorySerializer, ImageSerializer
 
-# Create your views here.
+
+# Для Image
+class ImageListCreateView(generics.ListCreateAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+
+class ImageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    lookup_field = "id"
+
+
+# Для Category
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = "id"
+
+
+# Для Card
+class CardListCreateView(generics.ListCreateAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+class CardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    lookup_field = "id"
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include("apps.users.urls", namespace='users')),
-    path('api/posts/', include('apps.catalog.urls', namespace='catalog')),
+    path('api/catalog/', include('apps.catalog.urls', namespace='catalog')),
 ]
